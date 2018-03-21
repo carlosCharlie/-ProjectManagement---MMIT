eae8d6356f1022a0e6cafa71925af5b19164494f
eduarrom <33223901+eduarrom@users.noreply.github.com>
2018-03-21 15:13:19 +0100

    Interfaz menu y listaequipos creadas y navegables

--- MMIT/src/main/java/com/mmit/presentacion/controladorVistaMenu.java
+++ MMIT/src/main/java/com/mmit/presentacion/controladorVistaMenu.java
@@ -5,9 +5,18 @@
  */
 package com.mmit.presentacion;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 
 /**
  * FXML Controller class
@@ -16,12 +25,25 @@ import javafx.fxml.Initializable;
  */
 public class controladorVistaMenu implements Initializable {
 
+    @FXML
+    private Button botonEquipos;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        try {
+            BorderPane root = (BorderPane) botonEquipos.getScene().getRoot();
+            root.setCenter((AnchorPane) FXMLLoader.load(getClass().getResource("/fxml/ListaEquipos.fxml")));
+        } catch (IOException ex) {
+            Logger.getLogger(controladorVistaMenu.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
 }
