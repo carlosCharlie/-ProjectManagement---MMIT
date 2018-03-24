2293735cef9e53ba38104eba38e68d2a272a04f1
MaPayo <marina.payorubio@gmail.com>
2018-03-24 18:35:32 +0100

    SAEquipos  Completo
    ListarEquipos añadido (no test)

--- MMIT/src/main/java/com/mmit/negocio/equipos/EquiposSAImp.java
+++ MMIT/src/main/java/com/mmit/negocio/equipos/EquiposSAImp.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mmit.negocio.equipos;
+
+import com.mmit.integracion.FactoriaIntegracion.FactoriaIntegracion;
+import com.mmit.integracion.equipos.EquiposDAO;
+import java.util.ArrayList;
 
 /**
  *
- * @author carlos
+ * @author MMIT
  */
 public class EquiposSAImp implements EquiposSA {
     
+    @Override
+    public ArrayList<EquipoTrans> listarEquipos(){
+        FactoriaIntegracion instancia =  FactoriaIntegracion.getInstancia();
+        EquiposDAO daoe= instancia.crearEquiposDAO();
+       
+    return daoe.listarEquipos();
+}
 }
