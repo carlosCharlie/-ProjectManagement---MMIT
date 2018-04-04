f255fa6f1927a0f2e598c973cb1e6dd57425af75
Eduardo Romero Palencia <eduarrom@ucm.es>
2018-04-04 16:16:05 +0200

    Buscar Entrenador Hecho

--- MMIT/src/main/java/com/mmit/presentacion/entrenadores/ControladorVistaEntrenadores.java
+++ MMIT/src/main/java/com/mmit/presentacion/entrenadores/ControladorVistaEntrenadores.java
@@ -1,7 +1,6 @@
 package com.mmit.presentacion.entrenadores;
 
 import com.mmit.negocio.entrenadores.TOAEntrenadorEquipo;
-import com.mmit.negocio.jugadores.TOAJugadorEquipo;
 import com.mmit.presentacion.Evento;
 import com.mmit.presentacion.controlador.Contexto;
 import com.mmit.presentacion.controlador.Controlador;
@@ -13,6 +12,10 @@ import java.util.Enumeration;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -20,6 +23,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TablePosition;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 
@@ -35,6 +39,10 @@ public class ControladorVistaEntrenadores implements Initializable {
     private TableColumn<TOAEntrenadorEquipo, Integer> colId;
     @FXML
     private TableColumn<TOAEntrenadorEquipo, String> colEquipo;
+    
+    private ObservableList<TOAEntrenadorEquipo> listaEntrenadores;
+    @FXML
+    private TextField buscar;
     /**
      * Initializes the controller class.
      */
@@ -54,12 +62,34 @@ public class ControladorVistaEntrenadores implements Initializable {
         Contexto contexto = new Contexto(Evento.ListarEntrenadores, null);
         Controlador.obtenerInstancia().accion(contexto);
         
-        ArrayList<TOAEntrenadorEquipo> listaEntrenadores = (ArrayList<TOAEntrenadorEquipo>) contexto.getDatos();
+        listaEntrenadores = FXCollections.observableArrayList((ArrayList<TOAEntrenadorEquipo>) contexto.getDatos());
         
         if (listaEntrenadores != null){
-            for (TOAEntrenadorEquipo entrenador : listaEntrenadores){
-                tablaEntrenadores.getItems().add(entrenador);
-            }
+            FilteredList<TOAEntrenadorEquipo> filteredData = new FilteredList<>(listaEntrenadores, p -> true);
+        
+            buscar.textProperty().addListener((observable, oldValue, newValue) -> {
+                filteredData.setPredicate(jugador-> {
+                    // If filter text is empty, display all persons.
+                    if (newValue == null || newValue.isEmpty()) {
+                        return true;
+                    }
+
+                    // Compare first name and last name of every person with filter text.
+                    String lowerCaseFilter = newValue.toLowerCase();
+
+                    if (jugador.getNombreEntrenador().toLowerCase().contains(lowerCaseFilter)) {
+                        return true; // Filter matches first name.
+                    } else if (jugador.getApellidosEntrenador().toLowerCase().contains(lowerCaseFilter)) {
+                        return true; // Filter matches first name.
+                    }
+                    return false; // Does not match.
+                });
+            });
+
+            SortedList<TOAEntrenadorEquipo> sortedData = new SortedList<>(filteredData);
+            sortedData.comparatorProperty().bind(tablaEntrenadores.comparatorProperty());
+
+            tablaEntrenadores.setItems(sortedData);
         } else {
             Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("");
@@ -69,6 +99,7 @@ public class ControladorVistaEntrenadores implements Initializable {
         }
     }
     
+    @FXML
     public void seleccionarEntrenador(){
        
         TablePosition pos = this.tablaEntrenadores.getSelectionModel().getSelectedCells().get(0);
