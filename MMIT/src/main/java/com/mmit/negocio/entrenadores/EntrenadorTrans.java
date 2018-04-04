cf044bc9d489e7c00581f377b61619cb1be80afd
MaPayo <marina.payorubio@gmail.com>
2018-04-04 21:34:05 +0200

    EntrenadorTrans completo
    Añadido Entrenadortrans con todos los campos

--- MMIT/src/main/java/com/mmit/negocio/entrenadores/EntrenadorTrans.java
+++ MMIT/src/main/java/com/mmit/negocio/entrenadores/EntrenadorTrans.java
@@ -8,8 +8,8 @@ public class EntrenadorTrans {
     private String apellidos;
     private Integer victorias;
     private Integer derrotas;
-    private Integer idEquipo;
     private Integer edad;
+    private Integer idEquipo;
     
     public EntrenadorTrans(Integer id, String nombre, String apellidos, Integer idEquipo){
         this.id = id;
@@ -18,33 +18,70 @@ public class EntrenadorTrans {
         this.idEquipo = idEquipo;
     }
 
-    public EntrenadorTrans(Integer id, String nombre, String apellidos, Integer victorias, Integer derrotas, Integer idEquipo) {
+    public EntrenadorTrans(Integer id, String nombre, String apellidos, Integer victorias, Integer derrotas, Integer edad, Integer idEquipo) {
         this.id = id;
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.victorias = victorias;
         this.derrotas = derrotas;
+        this.edad = edad;
         this.idEquipo = idEquipo;
     }
-    
-    public Integer getId() {
-        return id;
-    }
 
-    public String getNombre() {
-        return nombre;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public String getApellidos() {
-        return apellidos;
-    }
-    
-    public Integer getIdEquipo(){
-        return idEquipo;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public Integer getEdad() {
-        return edad;
-    }
-    
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public Integer getVictorias() {
+		return victorias;
+	}
+
+	public void setVictorias(Integer victorias) {
+		this.victorias = victorias;
+	}
+
+	public Integer getDerrotas() {
+		return derrotas;
+	}
+
+	public void setDerrotas(Integer derrotas) {
+		this.derrotas = derrotas;
+	}
+
+	public Integer getEdad() {
+		return edad;
+	}
+
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
+
+	public Integer getIdEquipo() {
+		return idEquipo;
+	}
+
+	public void setIdEquipo(Integer idEquipo) {
+		this.idEquipo = idEquipo;
+	}
+        
 }
