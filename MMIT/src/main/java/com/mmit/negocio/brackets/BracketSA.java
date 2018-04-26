1ab5c3ec8cad1d5c9b1d30784808712e75f53d3b
Eduardo Romero Palencia <eduarrom@ucm.es>
2018-04-26 08:05:56 +0200

    Transferido negocio otra historia

---
+++ MMIT/src/main/java/com/mmit/negocio/brackets/BracketSA.java
@@ -0,0 +1,12 @@
+
+package com.mmit.negocio.brackets;
+
+import com.mmit.negocio.equipos.EquipoTrans;
+
+public interface BracketSA {
+    /**
+     * Rellena el bracket inicial
+     * @return salida del programa
+     */
+   int loadInitBracket();
+}
---
+++ MMIT/src/main/java/com/mmit/negocio/brackets/BracketSAImp.java
@@ -0,0 +1,44 @@
+
+package com.mmit.negocio.brackets;
+
+import com.mmit.integracion.equipos.EquiposDAO;
+import com.mmit.integracion.factoriaIntegracion.FactoriaIntegracion;
+import com.mmit.negocio.equipos.EquipoTrans;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class BracketSAImp implements BracketSA {
+
+    
+    @Override
+    public int loadInitBracket() {
+        
+        try {
+        
+            EquiposDAO equiposDao = FactoriaIntegracion.getInstancia().crearEquiposDAO();
+            ArrayList<EquipoTrans> listaCompleta = equiposDao.readAll();
+            
+            ArrayList<EquipoTrans>listaFinal=new ArrayList<EquipoTrans>();
+            
+            
+            
+            //Coge el primero y el ultimo alternando de la lista de los 16 mejores
+            int N=16;
+            
+            for(int i=0;i<N/2;i++)
+            {
+                listaFinal.add(listaCompleta.get(i));
+                listaFinal.add(listaCompleta.get((N-1)-i));
+            }
+            
+            
+            return 0;
+            
+        } catch (Exception ex) {
+            return -5;
+        }
+    }
+    
+}
