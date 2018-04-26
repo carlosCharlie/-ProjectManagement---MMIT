259e3ed023515a91cb36528e8ccca46eb8170eb8
MaPayo <marina.payorubio@gmail.com>
2018-04-26 13:39:57 +0200

    Añadido bracketTrans a Login
    Añadido campo de tipo bracketTrans a la clase estatica Login

--- MMIT/src/main/java/com/mmit/negocio/usuarios/Login.java
+++ MMIT/src/main/java/com/mmit/negocio/usuarios/Login.java
@@ -1,22 +1,6 @@
-/*
- * Copyright (C) 2018 Your Organisation
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.mmit.negocio.usuarios;
 
-
+import com.mmit.negocio.brackets.BracketTrans;
 
 /**
  * Es una clase estatica que almacena el usuario actual de la aplicacion
@@ -25,7 +9,7 @@ public class Login {
     //En la base de datos hay insertado un admin por defecto: nombre="admin" password="adminP"
     
     static private UsuarioTrans usuario;
-
+    static private BracketTrans bracket;
 
     /**
      * Inicia una nueva sesion
@@ -41,6 +25,14 @@ public class Login {
      */
     public static UsuarioTrans getUsuario(){
         return usuario;
+    }
+    
+    public static void setBracket(BracketTrans bracket){
+    	Login.bracket = bracket;
+    }
+    
+    public static BracketTrans getBracket(){
+    	return bracket;
     }
   
 }
