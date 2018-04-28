1afc399c0a64122224e9e1dff9dfe11ffcaa98a2
MaPayo <marina.payorubio@gmail.com>
2018-04-28 22:02:54 +0200

    Test BracketDao Actualizado
    Test actualizado para cumplir el formato del código

--- MMIT/src/test/java/com/mmit/negocio/bracket/BracketDAOTestNG.java
+++ MMIT/src/test/java/com/mmit/negocio/bracket/BracketDAOTestNG.java
@@ -17,27 +17,26 @@ import com.mmit.negocio.usuarios.Login;
 public class BracketDAOTestNG {
 
 	@Test
-	public void loadInitBracketTestNG(){
+	public void loadInitBracketTestNG() {
 		System.out.println("Load bracket: Cargar correcto");
 		BracketSA bracket = FactoriaNegocio.getInstancia().crearBracketSA();
 		int resultado = bracket.loadInitBracket();
 		Assert.assertEquals(resultado, 0);
 	}
-	
-	
+
 	@Test
-	public void loadInitBracketResultTestNG(){
+	public void loadInitBracketResultTestNG() {
 		System.out.println("Load bracket: Comprobar resultados");
 		BracketSA bracket = FactoriaNegocio.getInstancia().crearBracketSA();
 		int resultado = bracket.loadInitBracket();
 		EquiposDAO equipos = FactoriaIntegracion.getInstancia().crearEquiposDAO();
-		try{ArrayList<EquipoTrans> equiposconsulta = equipos.readAll();
-		ArrayList<EquipoTrans> brackets = Login.getBracket();
-		Assert.assertEquals(brackets.get(0).getNombre(), equiposconsulta.get(0).getNombre());
+		try {
+			ArrayList<EquipoTrans> equiposconsulta = equipos.readAll();
+			ArrayList<EquipoTrans> brackets = Login.getBracket();
+			Assert.assertEquals(brackets.get(0).getNombre(), equiposconsulta.get(0).getNombre());
+		} catch (Exception ex) {
+			Assert.fail();
 		}
-		catch (Exception ex) {
-            Assert.fail();
-        }
 	}
-	
+
 }
