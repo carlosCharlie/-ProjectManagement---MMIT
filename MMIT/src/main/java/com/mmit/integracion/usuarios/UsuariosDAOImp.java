8c13bc0d2f855ebaf29532e363e3e665f213441a
Fernit <fertena@ucm.es>
2018-04-30 02:27:39 +0200

    dao usuario

--- MMIT/src/main/java/com/mmit/integracion/usuarios/UsuariosDAOImp.java
+++ MMIT/src/main/java/com/mmit/integracion/usuarios/UsuariosDAOImp.java
@@ -67,12 +67,10 @@ public class UsuariosDAOImp implements UsuariosDAO{
             Conexion.getInstancia().abrir();
             Connection c = Conexion.getInstancia().getResource();
           
-            PreparedStatement ps =  c.prepareStatement("Insert into usuarios values (?,?,?,?)");
+            PreparedStatement ps =  c.prepareStatement("Insert into usuarios (nombre,password,admin) values (?,?,false)");
         
-            ps.setInt(1,usuarioTrans.getId());
             ps.setString(1,usuarioTrans.getNombre());
             ps.setString(2,usuarioTrans.getPassword());
-            ps.setBoolean(3,usuarioTrans.getAdmin());
             ResultSet rs = ps.executeQuery();
         } catch (SQLException ex) {
             Logger.getLogger(UsuariosDAOImp.class.getName()).log(Level.SEVERE, null, ex);
