b960314235ed5ece6f66836a20e1e3e7607c077a
Dielam <dielagun@ucm.es>
2018-05-03 18:13:50 +0200

    Tests

--- MMIT/src/test/java/com/mmit/negocio/usuarios/UsuariosSATestNG.java
+++ MMIT/src/test/java/com/mmit/negocio/usuarios/UsuariosSATestNG.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.mmit.integracion.usuarios;
+package com.mmit.negocio.usuarios;
 
 import com.mmit.negocio.usuarios.UsuarioTrans;
 import com.mmit.negocio.usuarios.UsuariosSA;
